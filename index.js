e37ae9315253ff39bd09838fe2c3d6b69b550d6a
radeva <vesita@gmail.com>
2025-03-23 20:32:26 +0200

    add endpoints

--- index.js
+++ index.js
@@ -6,25 +6,41 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/:date?', function (req, res) {
+  const date = req.params.date;
+  let dateObj;
+  if (date == null) {
+    dateObj = new Date();
+  } else {
+    dateObj = new Date(date);
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    if (dateObj == 'Invalid Date') {
+      dateObj = new Date(parseInt(date));
+      if (dateObj == 'Invalid Date') {
+        res.json({ error: 'Invalid Date' });
+      }
+    }
+  }
+
+  res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() });
 });
 
-
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
